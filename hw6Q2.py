2daae5e581d00bde00318a00e6dc240013426ba5
fiorenza2 <philipjball93@gmail.com>
2016-11-02 19:40:08 +0000

    Add reg training

--- hw6Q2.py
+++ hw6Q2.py
@@ -27,17 +27,30 @@ def w_lin(psi_x,y):
     psi_sq = np.dot(psi_tr,psi_x)
     invert = np.linalg.inv(psi_sq)
     w = np.dot(np.dot(invert,psi_tr),y)
-    return w
+    return(w)
 
+def w_aug(psi_x,y,lamb):
+    psi_tr = np.transpose(psi_x)
+    psi_sq = np.dot(psi_tr,psi_x)
+    row,col = psi_sq.shape
+    reg_term = np.identity(row) * lamb
+    invert = np.linalg.inv(psi_sq + reg_term)
+    w = np.dot(np.dot(invert,psi_tr),y)
+    return(w)
+    
 # function which returns the in and out of sample errors for a given set of inputs and training schema
-def train_error_asses(in_s,out_s,w_train):
+    #note that if there is no lambda, set this to be "NA"
+def train_error_asses(in_s,out_s,w_train,lamb):
     x_feat_i = in_s[:,:2]   # get the features of the in sample
     x_feat_o = out_s[:,:2]  # get the features of the out sample
     y_i = in_s[:,2]     # get the classification of the in sample
     y_o = out_s[:,2]    # get the classification of the out sample
     psi_i = featurise(x_feat_i) # get the transformed feature vector in sample
     psi_o = featurise(x_feat_o) # get the transformed feature vector out sample
-    w_t = w_train(psi_i,y_i)    # get the weights by training on the in sample
+    if lamb == "NA":
+        w_t = w_train(psi_i,y_i)
+    else:
+        w_t = w_train(psi_i,y_i,lamb)
     y_i_pred = np.sign(np.dot(psi_i,w_t))    # calculate the in sample classification predictions
     y_o_pred = np.sign(np.dot(psi_o,w_t))    # calculate the out sample classification predictions
     class_e_i = np.mean(y_i != y_i_pred)
@@ -48,4 +61,5 @@ def train_error_asses(in_s,out_s,w_train):
 in_s = np.loadtxt('C:\\Users\\philip.ball\\Documents\\AI-DS\\edX CS1156x\\Python Scripts\\HW6Q2 Data\\in.dta')
 out_s = np.loadtxt('C:\\Users\\philip.ball\\Documents\\AI-DS\\edX CS1156x\\Python Scripts\\HW6Q2 Data\\out.dta')
 
-print(train_error_asses(in_s,out_s,w_lin))
+print(train_error_asses(in_s,out_s,w_lin,"NA"))
+print(train_error_asses(in_s,out_s,w_aug,10**1))
