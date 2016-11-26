7f405eb15fc2af8cb866f0d208160e8909fcfff2
fiorenza2 <philipjball93@gmail.com>
2016-11-26 22:20:09 +0000

    Add code for 8 and modify bias implementation

--- finalQ7.py
+++ finalQ7.py
@@ -16,18 +16,25 @@ def w_reg(Z,y,lamb):
     w = np.dot(pre,y)
     return(w)
     
-def lin_OvA(Z_in,y_in,lamb,num):
+def lin_OvA(Z_in,y_in,Z_out,y_out,lamb,num):
     ind = y_in == num   # vector which tells you which rows have the required number
-    bias = np.ones(y_in.size)
     y_new = np.ones(y_in.size)  # instantiate the classification vector
     y_new[~ind] = -1    # set the appropriate values to -1 (ie: the rows where the required number didn't appear)
-    Z_in = np.column_stack((bias,Z_in))  # add the bias terms
     w = w_reg(Z_in,y_new,lamb)
     y_hyp = np.sign(np.dot(w,np.transpose(Z_in)))
-    bin_err = y_hyp != y_new
+    bin_err_in = y_hyp != y_new
+    y_new_o,y_hyp_o = lin_OoS(Z_out,y_out,num,w)
+    bin_err_out = y_hyp_o != y_new_o
     #plot_OvA(Z_in,y_new,w)
     all0_ans = ind.sum()/ind.size
-    return(bin_err.mean(),all0_ans)
+    return(bin_err_in.mean(),bin_err_out.mean(),all0_ans)
+
+def lin_OoS(Z_out,y_out,num,w):
+    ind = y_out == num
+    y_new_o = np.ones(ind.size)
+    y_new_o[~ind] = -1
+    y_hyp_o = np.sign(np.dot(w,np.transpose(Z_out)))
+    return(y_new_o,y_hyp_o)
 
 def plot_OvA(Z_in,y_in,w):
     x = np.linspace(Z_in[:,1].min(),Z_in[:,1].max())
@@ -37,7 +44,16 @@ def plot_OvA(Z_in,y_in,w):
     plt.scatter(Z_in[~ind_pos][:,1],Z_in[~ind_pos][:,2],c = "green")
     plt.scatter(Z_in[ind_pos][:,1],Z_in[ind_pos][:,2],c = "red")
     plt.show()
-        
+
+# where n is the number of terms, order x0,x1,x2,x1x2,x1sq,x2sq
+def transform_Z(Z,n):
+    n,m = Z.shape
+    Z0 = np.ones(n)
+    Z_sq = Z**2
+    Z_cr = Z[:,0]*Z[:,1]
+    Z_trans = np.column_stack((Z0,Z,Z_cr,Z_sq))
+    return(Z_trans[:,0:n])
+    
 # the below code was used to verify that my code was working as expected
 #y_test = np.array([1,1,1,1,-1,-1,-1,-1])
 #Z_test = np.array([1,2,1,2,5,6,5,6])
@@ -69,28 +85,51 @@ def plot_OvA(Z_in,y_in,w):
 #y_in = feat_train[:,0]
 #Z_in = feat_train[:,1:]
 #Z_in[:,1] = Z_in[:,1]**2
+#r,c = Z_in.shape
+#bias = np.ones(r)
+#Z_in = np.column_stack((bias,Z_in))
 #
 #nums = [0,1,2,3,4]
 #lamb = 0.5
 #
 #for n in nums:
-#    err,test = lin_OvA(Z_in,y_in,lamb,n)
+#    err,erroos,test = lin_OvA(Z_in,y_in,Z_in,y_in,lamb,n)
 #    print("In sample error for " + str(n)+ ":" + str(err))
 #    print("All -1 guess: "+  str(test))
 
 
-#here begins the code for the finals question
+#here begins the code for the finals question 7
+
+#feat_test = np.loadtxt('C:\\Users\\philip.ball\\Documents\\AI-DS\\edX CS1156x\\Python Scripts\\HW8Q2 Data\\features.test.txt')
+#feat_train = np.loadtxt('C:\\Users\\philip.ball\\Documents\\AI-DS\\edX CS1156x\\Python Scripts\\HW8Q2 Data\\features.train.txt')
+#
+#y_in = feat_train[:,0]
+#Z_in = transform_Z(feat_train[:,1:],3)
+#y_out = feat_test[:,0]
+#Z_out = transform_Z(feat_test[:,1:],3)
+#
+#nums = [5,6,7,8,9]
+#lamb = 1
+#
+#for n in nums:
+#    err,erroos,test = lin_OvA(Z_in,y_in,Z_out,y_out,lamb,n)
+#    print("In sample error for " + str(n)+ ":" + str(err))
+#    print("All 0 error: " + str(test))
+
+# finals question 8
 
 feat_test = np.loadtxt('C:\\Users\\philip.ball\\Documents\\AI-DS\\edX CS1156x\\Python Scripts\\HW8Q2 Data\\features.test.txt')
 feat_train = np.loadtxt('C:\\Users\\philip.ball\\Documents\\AI-DS\\edX CS1156x\\Python Scripts\\HW8Q2 Data\\features.train.txt')
 
-y_in = feat_test[:,0]
-Z_in = feat_test[:,1:]
+y_in = feat_train[:,0]
+Z_in = transform_Z(feat_train[:,1:],6)
+y_out = feat_test[:,0]
+Z_out = transform_Z(feat_test[:,1:],6)
 
-nums = [5,6,7,8,9]
+nums = [0,1,2,3,4]
 lamb = 1
 
 for n in nums:
-    err,test = lin_OvA(Z_in,y_in,lamb,n)
-    print("In sample error for " + str(n)+ ":" + str(err))
-
+    err,erroos,test = lin_OvA(Z_in,y_in,Z_out,y_out,lamb,n)
+    print("Out of sample error for " + str(n)+ ":" + str(erroos))
+    #print("All 0 error: " + str(test))
